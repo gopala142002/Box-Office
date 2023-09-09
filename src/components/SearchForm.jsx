c4dbde6aa49737006fb2d1eb14c89a58ad653547
Gopala <gopalapatel1234@gmail.com>
2023-09-09 18:21:10 +0530

    persisted searchForm tectbox value in session storage. write all the logic in useSearchStr hook

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,7 @@
 import { useState} from "react";
+import { useSearchStr } from "../lib/useSearchStr";
 const SearchForm = ({ onSearch }) => {
-  const [searchStr, setSearchStr] = useState("");
+  const [searchStr, setSearchStr] = useSearchStr("");
   const [searchOption, setSearchOption] = useState("shows");
   const onSearchInput = (ev) => {
     setSearchStr(ev.target.value);
